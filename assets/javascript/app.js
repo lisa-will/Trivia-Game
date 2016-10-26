217e09573ea96b575433cc195dfb486cd0111879
Lisa Williams <williams.lisarenee@gmail.com>
2016-10-25 21:56:25 -0400

    added jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,22 +29,22 @@ function startTrivia(){
 
 $("#startTrivia").on("click", function(){
 	startTrivia();
+	renderQuestion();
 })
 
 // Progress bar function to show tester trivia progress... 
 
 // * Still needs some work. 
 
-updateProgressBar(5)
 var progression = 0;
 
-function updateProgressBar(progress) { 
+function updateProgressBar() { 
 		$('#progress .progress-text').text(progression + '%');
 		$('#progress .progress-bar').css({'width': progression+'%'});
 		if(progression == 100) {
-			var progression = 0
+	//fill
 		} else {
-			progression += progress;
+			progression += 5;
 		}
 
 };
@@ -54,22 +54,14 @@ function updateProgressBar(progress) {
 // Let's Add our Trivia Variables.... Annnnd Go! 
 // * Add Trivia-Q' images. 
 
-// Add function ...
-/* $(document).ready(function() {
- 	 var triviaQ =  ['assets/images/1.png',
- 					'assets/images/2.png',
- 					'assets/images/3.png',
- 					'assets/images/4.png',
- 					'assets/images/5.png',
- 					'assets/images/6.png',]; */
 
 var q = 0, trivia, triviaStatus, question, choice, choices, ansA, ansB, ansC, ansD, correct, wrong = 0; 
 
 var triviaQuestions = [
 //Q1 
-	["assets/images/1.png", "A", "B", "C", "D", "A"],
+	["assets/images/1.png", "A.", "B.", "C.", "D.", "A"],
 //Q2
-	["Blah Blah Blah", "A. Method", "B. Variable", "C. Object", "D. Function", "D"],
+	["assets/images/2.png", "A. Method", "B. Variable", "C. Object", "D. Function", "D"],
 //Q3
 
 //Q4	
@@ -89,26 +81,28 @@ function renderQuestion() {
 // This will tally up number of answer right! 
 	if(q >= triviaQuestions.length)	{
 		trivia.innerHTML = "<h2>You got "+correct+" of "+triviaQuestions+" questions correct!</h2>";
-		_("triviaStatus").innerHTML = "Trivia Over";
+		$("#triviaStatus").innerHTML = "Trivia Over";
 		q = 0;
 		correct = 0; 
 		wrong = 0; 
 		return false;
 	}
-	_("triviaStatus").innerHTML = "Question " +(q+1)+" of "+triviaQuestions.length;
+	$("#triviaStatus").html("Question " +(q+1)+" of "+triviaQuestions.length);
 	question = triviaQuestions[q][0];
 	ansA = triviaQuestions[q][1];
 	ansB = triviaQuestions[q][2];
 	ansC = triviaQuestions[q][3];
 	ansD = triviaQuestions[q][4];
 // This displays the trivia question. 
-	trivia.innerHTML = "<img src='"+question+"'>";
+	trivia.html("<img id='pic' src='"+question+"'><br>");
 // This displays answer selections with "radio-style" button.
-	trivia.innerHTML += "<input type='radio' name='choices' value='A'> "+ansA+"<br>";
-	trivia.innerHTML += "<input type='radio' name='choices' value='B'> "+ansB+"<br>";
-	trivia.innerHTML += "<input type='radio' name='choices' value='C'> "+ansC+"<br>";
-	trivia.innerHTML += "<input type='radio' name='choices' value='D'> "+ansD+"<br><br>";
-	trivia.innerHTML += "<button onclick='checkAnswer()'>Sumbit Answer</button>";
+	trivia.append("<input type='radio' name='choices' value='A'> "+ansA+"<br>");
+	trivia.append("<input type='radio' name='choices' value='B'> "+ansB+"<br>");
+	trivia.append("<input type='radio' name='choices' value='C'> "+ansC+"<br>");
+	trivia.append("<input type='radio' name='choices' value='D'> "+ansD+"<br><br>");
+	trivia.append("<button onclick='checkAnswer()'>Sumbit Answer</button>");
+
+	updateProgressBar()
 }
 
 function checkAnswer() {
