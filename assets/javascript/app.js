4346b561a519d903e338a22b1cabd2d60110e276
Lisa Williams <williams.lisarenee@gmail.com>
2016-10-26 13:01:18 -0400

    added trivia images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,22 +59,50 @@ var q = 0, trivia, triviaStatus, question, choice, choices, ansA, ansB, ansC, an
 
 var triviaQuestions = [
 //Q1 
-	["assets/images/1.png", "A.", "B.", "C.", "D.", "A"],
+	["assets/images/1.png", "A. Incrementor", "B. Operator", "C. Iterator", "D. Decrementor", "A"],
 //Q2
-	["assets/images/2.png", "A. Method", "B. Variable", "C. Object", "D. Function", "D"],
+	["assets/images/2.png", "A. Variables", "B. Parameters", "C. Strings", "D. Object", "B"],
 //Q3
-
+	["assets/images/3.png", "A. Method", "B. Variable", "C. Object", "D. Function", "D"],
 //Q4	
-	
+	["assets/images/4.png", "A. Variable", "B. String", "C. Object", "D. Method", "A"],
 //Q5
-
-
-	
+	["assets/images/5.png", "A. Function", "B. Object", "C. For Loop", "D. While Loop", "C"],
+//Q6
+	["assets/images/6.png", "A. Property", "B. Variable", "C. Value", "D. String", "A"],
+//Q7
+	["assets/images/7.png", "A. Object Literal Notation", "B. Bracket Notation", "C. Dot Notation", "D. Object Literal Notation", "B"],
+//Q8
+	["assets/images/8.png", "A. Array", "B. Function", "C. Object", "D. Method", "A"],
+//Q9
+	["assets/images/9.png", "A. While Loop", "B. Incrementor", "C. For Loop", "D. Function", "A"],	
+//Q10	
+	["assets/images/10.png", "A. Dot Notation", "B. Object Literal Notation", "C. Bracket Notation", "D. Object Constructor Notation", "B"],
+//Q11
+	["assets/images/11.png", "A. If... Else Statement", "B. Ternary Operator", "C. Else If Statement", "D. Switch Statement", "D"],
+//Q12
+	["assets/images/12.png", "A. If Statement", "B. If... Else Statement", "C. Else If Statement", "D. Switch Statement", "B"],		
+//Q13
+	["assets/images/13.png", "A. Dot Notation", "B. Bracket Notation", "C. Object Literal Notation", "D. Object Constructor Notation", "A"],	
+//Q14
+	["assets/images/14.png", "A. If Statement", "B. If...Else Statement", "C. Switch Statement", "D. Ternary Statement", "D"],
+//Q15
+	["assets/images/15.png", "A. Method", "B. Object", "C. Function", "D. Variable", "C"],	
+//Q16
+	["assets/images/16.png", "A. Object Literal Notation", "B. Object Constructor Notation", "C. Dot Notation", "D. Bracket Notation", "B"],
+//Q17
+	["assets/images/17.png", "A", "B", "C", "D", "A"],
+//Q18
+	["assets/images/18.png", "A", "B", "C", "D", "D"],
+//Q19
+	["assets/images/19.png", "A", "B", "C", "D", "D"],
+//Q20
+	["assets/images/20.png", "A", "B", "C", "D", "B"],		
 ];
 // This function returns the element Id when it is called. 
-function _(x) {
-	return document.getElementById(x);
-}
+//function _(x) {
+	//return document.getElementById(x);
+//}
 // This renderQuestion function pulls the question + answer choices from the triviaQuestions array. 
 function renderQuestion() {
 	trivia = $("#trivia");
